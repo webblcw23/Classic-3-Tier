fd7c3aaa9463a3b6fce3539ac76366ea7886c225
Lewis Webb <webblcw23@hotmail.com>
2025-10-28 06:19:57 +0000

    dockerfile to Dockerfile

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -17,8 +17,9 @@ provider "azurerm" {
       prevent_deletion_if_contains_resources = false
     }
   }
-  use_oidc = true
+  subscription_id = "91c0fe80-4528-4bf2-9796-5d0f2a250518"
 }
+
 
 provider "azuread" {
   use_oidc = true
